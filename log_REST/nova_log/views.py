6a2b34496b808ced1abcd3bf5b69f5e294d35043
haminhcong <haminhcongbkhn@gmail.com>
2016-08-30 19:38:09 +0700

    Update

--- log_REST/nova_log/views.py
+++ log_REST/nova_log/views.py
@@ -33,9 +33,10 @@ def nova_log_view(request):
             return HttpResponse(json.dumps({"result": "invalid value"}), status=404)
         except ValueError:
             return HttpResponse(json.dumps({"result": "invalid value"}), status=404)
-    t = datetime.datetime.now()
-    check_time = datetime.datetime(t.year,t.month,t.day,00,00,00)+datetime.timedelta(days=1,hours=1)
-    if (start_date and end_date and start_date>end_date)or (end_date and end_date>check_time):
+    # t = datetime.datetime.now()
+    # check_time = datetime.datetime(t.year,t.month,t.day,00,00,00)+datetime.timedelta(days=1,hours=1)
+    # if (start_date and end_date and start_date>end_date)or (end_date and end_date>check_time):
+    if (start_date and end_date and start_date > end_date):
         return HttpResponse(json.dumps({"result": "invalid value"}), status=404)
     log_reader = readfile.ReadLog("/home/cong/n-api.log")
     log_summary_result = log_reader.summary_log_by_day(start_date, end_date)
